40fadada0d05aee4f1ca46369cc121ae33fd0468
Ricardo Duarte <127055071+FXgamerPT@users.noreply.github.com>
2024-01-08 12:03:00 +0000

    Python_app_1.0.2
    Mais frames e cores novas

--- PythonFile/teste.py
+++ PythonFile/teste.py
@@ -10,19 +10,38 @@ class Dashboard:
         self.window.title("Rissois")
         self.window.geometry("1366x768")
         self.window.state("zoomed")
-        self.window.configure(background="lightblue")
+        self.window.configure(background="#232323")
 
             #icon da janela
         icon = PhotoImage(file="../imagens/profile.png")
         self.window.iconphoto(True, icon)
 
-        #Cabecalho
-        self.header = Frame(self.window, bg ="#009df4")
-        self.header.place(x=300,y=0,width=1070,height=60)
+        #Barra Lateral
+        self.sidebar = Frame(self.window, bg="#2E3133")
+        self.sidebar.place(x=0,y=0,width=300,height=1900)
 
-            #botão de sair
-        self.logout_text =Button(self.window,text="Sair", bg="#32cf8e" , font =("" , 13 ,"bold"),bd=0, fg='white', cursor = 'hand2' , activebackground='#32cf8e', command=self.window.destroy)
-        self.logout_text.place(x=1315,y=15)
+        #Corpo
+        self.heading = Label(self.window,text="Dashboard" , font=("",13,"bold"),fg='white',bg='#232323')
+        self.heading.place(x=325,y=50)
+
+        self.line = Label(self.window, text="____________", font=("", 10, "bold"), fg='#ffb366', bg='#232323')
+        self.line.place(x=325, y=25)
+
+            #Frame 1 do body
+        self.bodyFrame1= Frame(self.window,bg="#2E3133")
+        self.bodyFrame1.place(x=328,y=110,width=1010,height=350)
+
+            #Frame 2 do body
+        self.bodyFrame2= Frame(self.window,bg="#2E3133")
+        self.bodyFrame2.place(x=328,y=495,width=310,height=200)
+
+            #Frame 3 do body
+        self.bodyFrame3= Frame(self.window,bg="#2E3133")
+        self.bodyFrame3.place(x=680,y=495,width=310,height=200)
+
+            #Frame 2 do body
+        self.bodyFrame4= Frame(self.window,bg="#2E3133")
+        self.bodyFrame4.place(x=1030,y=495,width=310,height=200)
 
 def win():
     window = Tk()
